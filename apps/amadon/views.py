6f7e87403cc17c4f7d232b9950ac437991fd9c48
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2017-08-14 20:07:29 -0700

    Finished the assignment

---
+++ apps/amadon/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+# the index function is called when root is visited
+def index(request):
+    return render(request, "amadon/index.html")
+
+def buy(request):
+    if request.method == "POST":
+        prod_id = int(request.POST['product_id'])
+        quant = int(request.POST['quantity'])
+        
+        if prod_id == 1:
+            prod_price = 19.99
+        if prod_id == 2:
+            prod_price = 29.99
+        if prod_id == 3:
+            prod_price = 4.99
+        if prod_id == 4:
+            prod_price = 49.99
+
+        order_price = prod_price * quant
+
+        request.session['order_price'] = order_price
+
+        if "count" in request.session:
+            request.session['count'] += quant
+            request.session['total'] += order_price
+        else:
+            request.session['count'] = quant
+            request.session['total'] = order_price
+
+        return redirect("/amadon/checkout")
+
+def checkout(request):
+    return render(request, "amadon/thanks.html")
+
+def reset(request):
+    request.session.clear()
+    return redirect("/")
